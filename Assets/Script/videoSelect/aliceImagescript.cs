09c3651b7680b607eada6661b949de8f9fe26e87
amirabil <anthony.mirabile@inria.fr>
2020-02-21 16:25:10 +0100

    oubli script videoSelect

---
+++ Assets/Script/videoSelect/aliceImagescript.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class aliceImagescript : MonoBehaviour
+{
+    public VideoManager manager;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "main") { manager.ChangeVideo("rein4"); }
+
+        Debug.Log(other);
+    }
+}
---
+++ Assets/Script/videoSelect/itwImageScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class itwImageScript : MonoBehaviour
+{
+    public VideoManager manager;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "main") { manager.ChangeVideo("itw"); }
+    }
+}
